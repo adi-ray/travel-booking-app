772db500efc667023d0de4b0238631b74bdd59b2
adi-ray <adityadns03@gmail.com>
2025-04-13 04:33:09 +0530

    fix: build issues

--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -1,25 +1,32 @@
-import express from 'express';
-import { createTour, deleteTour, getAllTours, getFeaturedTour, getSingleTour, getTourBySearch, updateTour,getTourCount } from '../controllers/tourController.js';
-import { verifyAdmin } from '../Utils/verifyToken.js';
+import express from "express";
+import {
+  createTour,
+  deleteTour,
+  getAllTours,
+  getFeaturedTour,
+  getSingleTour,
+  getTourBySearch,
+  updateTour,
+  getTourCount,
+} from "../controllers/tourController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 const router = express.Router();
 
 //create New Tour
-router.post('/createTour',verifyAdmin,createTour);
+router.post("/createTour", verifyAdmin, createTour);
 //Update Existing Tour
-router.put('/:id',verifyAdmin,updateTour);
+router.put("/:id", verifyAdmin, updateTour);
 //delete Tour
-router.delete('/:id',verifyAdmin,deleteTour);
+router.delete("/:id", verifyAdmin, deleteTour);
 //getSingle Tour
-router.get('/getSingleTour/:id',getSingleTour);
+router.get("/getSingleTour/:id", getSingleTour);
 //getAllTour Tour
-router.get('/getAllTour',getAllTours);
+router.get("/getAllTour", getAllTours);
 //getTourBySearch
-router.get('/search/getTourBySearch',getTourBySearch);
+router.get("/search/getTourBySearch", getTourBySearch);
 //getFeaturedTour
-router.get('/search/getFeaturedTour',getFeaturedTour);
+router.get("/search/getFeaturedTour", getFeaturedTour);
 //getTourCount
-router.get('/search/getTourCount',getTourCount);
-
-
+router.get("/search/getTourCount", getTourCount);
 
 export default router;
